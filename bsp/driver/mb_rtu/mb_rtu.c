275d60ead9cafa99aecf664af31dbc7758ecc4bf
jsq <jsq234@163.com>
2019-07-20 12:30:47 +0800

    add the libmodbus driver and test code.

--- bsp/driver/mb_rtu/mb_rtu.c
+++ bsp/driver/mb_rtu/mb_rtu.c
@@ -1,15 +1,23 @@
 #include "mb_rtu.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "modbus.h"
 
 
 void *mb_rtu_master_thread(void *arg)
 {
+	uint16_t tab_reg[100] = {0};
+	modbus_t *ctx = NULL;
+	int rc, i;
+
 	DEV_PARA *pdev;
 	pdev = (DEV_PARA *)arg;
 
 	Pthread_detach(pthread_self());
-
-	while(1){
-		printf("%d|", pdev->index);
+	/* print the arg */
+	printf("%d|", pdev->index);
 		printf("%s|", pdev->name);
 		printf("%s|", pdev->driver);
 		printf("%d|", pdev->rem_para1);
@@ -29,8 +37,43 @@ void *mb_rtu_master_thread(void *arg)
 		printf("%d|", pdev->scan_cycle);
 		printf("%d|", pdev->log); 
 		printf("%d\n", pdev->repeat);
+	//init the libmodbus
+	ctx = modbus_new_rtu("/dev/ttySP0", 9600, 'N', 8, 1);
+	if(ctx == NULL){
+		printf("init the libmodbus error.\n");
+		return -1;
+	}
+
+	modbus_set_debug(ctx, 1);  //enable the debug info
+	modbus_set_slave(ctx, 1);  //set slave id
+
+	//connect the device
+	if(modbus_connect(ctx) == -1){
+		fprintf(stderr, "connect dev failed:%s\n", modbus_strerror(errno));
+		modbus_close(ctx);
+		modbus_free(ctx);
+		return -1;
+	}
+
+
+	while(1){
+		printf("\n---------------------------\n");
+		rc = modbus_read_registers(ctx, 0, 10, tab_reg);
+		if(rc == -1){
+			fprintf(stderr, "%s\n", modbus_strerror(errno));
+			modbus_close(ctx);
+			modbus_free(ctx);
+			return -1;
+		}
+
+		for(i = 0; i < 10; i++)
+			printf("reg[%d] = %d(0x%x)\n", i, tab_reg[i], tab_reg[i]);
 
 		sleep(2);
 	}
+
+	modbus_close(ctx);
+	modbus_free(ctx);
+
 	return 0;
 }
