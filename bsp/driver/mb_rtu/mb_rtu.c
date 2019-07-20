71fe2beaa537caaf1b0870a5f441c82ef7d129df
jsq <jsq234@163.com>
2019-07-20 17:59:56 +0800

    modify all 1800.

--- bsp/driver/mb_rtu/mb_rtu.c
+++ bsp/driver/mb_rtu/mb_rtu.c
@@ -8,35 +8,23 @@
 
 void *mb_rtu_master_thread(void *arg)
 {
-	uint16_t tab_reg[100] = {0};
 	modbus_t *ctx = NULL;
-	int rc, i;
+	int rc, cnt = 0;
+	int i,j;
 
-	DEV_PARA *pdev;
-	pdev = (DEV_PARA *)arg;
+	_THREAD_PARA *pdev;
+	_RTU_DRIVER *rtu_para = NULL;
+	pdev = (_THREAD_PARA *)arg;
 
 	Pthread_detach(pthread_self());
 	/* print the arg */
-	printf("%d|", pdev->index);
-		printf("%s|", pdev->name);
-		printf("%s|", pdev->driver);
-		printf("%d|", pdev->rem_para1);
-		printf("%s|", pdev->rem_para2);
-		printf("%s|", pdev->rem_para3);
-		printf("%s|", pdev->loc_para1);
-		printf("%s|", pdev->loc_para2);
-		printf("%d|", pdev->com_index);
-		printf("%d|", pdev->data_type);
-		printf("%s|", pdev->rw_type);
-		printf("%s|", pdev->unit_para1);
-		printf("%s|", pdev->unit_para2);
-		printf("%d|", pdev->data_para);
-		printf("%d|", pdev->start_addr);
-		printf("%d|", pdev->unit_len);
-		printf("%d|", pdev->data_hold);
-		printf("%d|", pdev->scan_cycle);
-		printf("%d|", pdev->log); 
-		printf("%d\n", pdev->repeat);
+	for(i = 0; i < 5; i++){
+		rtu_para = pdev->rtu_data[i];
+		printf("pdev[%d]->slave_id:%d\n", i, rtu_para->slave_id);
+		printf("pdev[%d]->start_addr:%d\n", i, rtu_para->start_addr);
+		printf("pdev[%d]->unit_len:%d\n", i, rtu_para->unit_len);
+		printf("pdev[%d]->cycle:%d\n", i, rtu_para->cycle);
+	}
 	//init the libmodbus
 	ctx = modbus_new_rtu("/dev/ttySP0", 9600, 'N', 8, 1);
 	if(ctx == NULL){
@@ -44,7 +32,7 @@ void *mb_rtu_master_thread(void *arg)
 		return -1;
 	}
 
-	modbus_set_debug(ctx, 1);  //enable the debug info
+	modbus_set_debug(ctx, pdev->slave_id);  //enable the debug info
 	modbus_set_slave(ctx, 1);  //set slave id
 
 	//connect the device
@@ -58,16 +46,21 @@ void *mb_rtu_master_thread(void *arg)
 
 	while(1){
 		printf("\n---------------------------\n");
-		rc = modbus_read_registers(ctx, 0, 10, tab_reg);
-		if(rc == -1){
-			fprintf(stderr, "%s\n", modbus_strerror(errno));
-			modbus_close(ctx);
-			modbus_free(ctx);
-			return -1;
+		for(i = 0; i < 5; i++){
+			rtu_para = pdev->rtu_data[i];
+			modbus_set_slave(ctx, rtu_para->slave_id);
+			rc = modbus_read_registers(ctx, rtu_para->start_addr,
+									rtu_para->unit_len, rtu_para->rx_buf);
+			if(rc == -1){
+				fprintf(stderr, "%s\n", modbus_strerror(errno));
+				modbus_close(ctx);
+				modbus_free(ctx);
+				return -1;
+			}else{
+				for(j = 0; j < 10; j++)
+					printf("reg[%d] = %d(0x%x)\n", j, rtu_para->rx_buf[j], rtu_para->rx_buf[j]);
+			}
 		}
-
-		for(i = 0; i < 10; i++)
-			printf("reg[%d] = %d(0x%x)\n", i, tab_reg[i], tab_reg[i]);
 
 		sleep(2);
 	}
